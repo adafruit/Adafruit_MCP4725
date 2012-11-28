7ea52614ca7625d020ec71f2d40ab49bdafa8f2a
K. Townsend <kevin@adafruit.com>
2012-11-28 14:06:48 +0100

    Fixed missing brackets

--- Adafruit_MCP4725.cpp
+++ Adafruit_MCP4725.cpp
@@ -76,7 +76,7 @@ void Adafruit_MCP4725::setVoltage( uint16_t output, bool writeEEPROM )
     Wire.write(MCP4726_CMD_WRITEDAC);
   }
   Wire.write(output / 16);                   // Upper data bits          (D11.D10.D9.D8.D7.D6.D5.D4)
-  Wire.write(output % 16) << 4;              // Lower data bits          (D3.D2.D1.D0.x.x.x.x)
+  Wire.write((output % 16) << 4);            // Lower data bits          (D3.D2.D1.D0.x.x.x.x)
   Wire.endTransmission();
   TWBR = twbrback;
 }
